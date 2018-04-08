3480ca71309c64c833d10183dc8bdf010c09658e
papago89 <papago2683@gmail.com>
2018-04-08 16:01:39 +0800

    coding style fix

--- RSSDiscordSender.js
+++ RSSDiscordSender.js
@@ -1,40 +1,38 @@
-const RSSFeedFilter = require("./RSSFeedFilter");
-﻿const Discord = require("discord.js");
+const RSSFeedFilter = require('./RSSFeedFilter');
+const Discord = require('discord.js');
 
-class RSSDiscordSender extends RSSFeedFilter{
+class RSSDiscordSender extends RSSFeedFilter {
+  constructor(channel, filter = {}, cycleMilliseconds = 60000) {
+    super(filter, cycleMilliseconds);
+    this.channel = channel;
+  }
 
-    constructor(channel, filter = {}, cycleMilliseconds = 60000) {
-        super(filter, cycleMilliseconds);
-        this.channel = channel;
-    }
+  addList(url) {
+    this.feederList.add({
+      url: url,
+      refresh: this.cycleMilliseconds
+    });
 
-    addList(url) {
-        this.feederList.add({
-            url: url,
-            refresh: this.cycleMilliseconds
-        });
-
-        // avoid RSS init status, too much new item.
-        setTimeout(() =>{
-            this.feederList.on('new-item', function(item) {
-                if (this.checkItem(item)) {
-                    let message = `${item.title}
+    // avoid RSS init status, too much new item.
+    setTimeout(() => {
+      this.feederList.on('new-item', function(item) {
+        if (this.checkItem(item)) {
+          const message = `${item.title}
 ${item.description}
 ${item.link}`;
 
-                    let embed = new Discord.RichEmbed()
-                        .setTitle("news")
-                        .setThumbnail("http://i.imgur.com/T4y0egb.jpg")
-                        .setColor(3447003)
-                        .addField("雪乃からの伝言", message)
-                        .setFooter("比企谷雪乃")
-                        .setTimestamp();
-                    this.channel.sendMessage(embed);
-                }
-            }, this);
-        }, 20000);
-    }
-
+          const embed = new Discord.RichEmbed()
+            .setTitle('news')
+            .setThumbnail('http://i.imgur.com/T4y0egb.jpg')
+            .setColor(3447003)
+            .addField('雪乃からの伝言', message)
+            .setFooter('比企谷雪乃')
+            .setTimestamp();
+          this.channel.send(embed);
+        }
+      }, this);
+    }, 20000);
+  }
 }
 
 module.exports = RSSDiscordSender;
